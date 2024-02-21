96a5ec54fef8ab4707f31d3a78f37f43ef695497
Meerata <merata87@gmail.com>
2024-02-21 15:28:46 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your code in this file!
+// index.js
+
+// Define the currentUser variable
+const currentUser = "Grace Hopper";
+
+// Define the welcomeMessage variable
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Define the excitedWelcomeMessage variable
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Define the shortGreeting variable
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+// Export variables
+module.exports = {
+  currentUser,
+  welcomeMessage,
+  excitedWelcomeMessage,
+  shortGreeting,
+};
